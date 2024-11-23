66fd869b9444d0a9129b22d18f423fea98467c37
oreximus <oreximus@gmail.com>
2024-11-23 06:36:48 +0530

    updating with a portfolio website setup

--- src/components/banner/Media.jsx
+++ src/components/banner/Media.jsx
@@ -1,6 +1,6 @@
-import React from 'react';
+import React from "react";
 import { BsGithub } from "react-icons/bs";
-import { FaInstagram, FaLinkedinIn, FaReact } from "react-icons/fa";
+import { FaLinkedinIn, FaReact, FaTwitter } from "react-icons/fa";
 import { IoLogoJavascript } from "react-icons/io";
 import { RiHtml5Fill } from "react-icons/ri";
 
@@ -10,37 +10,63 @@ const Media = () => {
       <div>
         <h2 className="text-base uppercase font-titleFont mb-4">Find me on</h2>
         <div className="flex gap-4">
-          <a href="https://github.com/Engglala243" target="_blank" rel="noopener noreferrer">
+          <a
+            href="https://github.com/oreximus/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             <span className="bannerIcon">
               <BsGithub />
             </span>
           </a>
-          <a href="https://www.linkedin.com/in/aditya-patidar-9349a3218/" target="_blank" rel="noopener noreferrer">
+          <a
+            href="https://www.linkedin.com/in/anubhav-singh-parte-b65244190/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             <span className="bannerIcon">
               <FaLinkedinIn />
             </span>
           </a>
-          <a href="https://www.instagram.com/aditya_patidar_37/#" target="_blank" rel="noopener noreferrer">
+          <a
+            href="https://x.com/oreximus"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             <span className="bannerIcon">
-              <FaInstagram />
+              <FaTwitter />
             </span>
           </a>
         </div>
       </div>
       <div>
-        <h2 className="text-base uppercase font-titleFont mb-4">Best Skills On</h2>
+        <h2 className="text-base uppercase font-titleFont mb-4">
+          Best Skills On
+        </h2>
         <div className="flex gap-4">
-          <a href="https://react.dev/learn" target="_blank" rel="noopener noreferrer">
+          <a
+            href="https://react.dev/learn"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             <span className="bannerIcon">
               <FaReact />
             </span>
           </a>
-          <a href="https://yesicon.app/ri/html5-fill" target="_blank" rel="noopener noreferrer">
+          <a
+            href="https://yesicon.app/ri/html5-fill"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             <span className="bannerIcon">
               <RiHtml5Fill />
             </span>
           </a>
-          <a href="https://www.w3schools.com/js/DEFAULT.asp" target="_blank" rel="noopener noreferrer">
+          <a
+            href="https://www.w3schools.com/js/DEFAULT.asp"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             <span className="bannerIcon">
               <IoLogoJavascript />
             </span>
